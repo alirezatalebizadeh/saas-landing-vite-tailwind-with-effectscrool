7c2c944ff240a7d9eb45cd7d400bd55147b96cd8
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2025-02-07 23:36:06 +0330

    debuging

--- saas-landing/src/sections/Hero.jsx
+++ saas-landing/src/sections/Hero.jsx
@@ -1,11 +1,10 @@
-import { Element, Link as LinkScroll } from "react-scroll";
 import Button from "../components/Button";
 
 
 const Hero = () => {
   return (
     <section className="relative pt-30 md:pt-40 pb-60 max-lg:pb-36 max-md:pb-32">
-      <Element name="hero">
+      <div id="hero">
         <div className="container relative">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
             <div className="caption small-2 uppercase text-[#C8EA80]">
@@ -17,9 +16,9 @@ const Hero = () => {
             <p className="max-w-440 mb-14 body-1 max-md:mb-10">
               ما ویرایشگر ویدیو برنامه را به‌گونه‌ای طراحی کرده‌ایم که استفاده از آن آسان، یادگیری سریع و به طرز شگفت‌انگیزی قدرتمند باشد.
             </p>
-            <LinkScroll to="features" offset={-100} spy smooth>
+            <a href="#services">
               <Button icon="/images/zap.svg">همین حالا امتحان کن</Button>
-            </LinkScroll>
+            </a>
           </div>
 
           <div className="absolute -top-30 right-[calc(50%-240px)]  md:right-[calc(50%-140px)] md:-top-50 lg:right-[calc(50%-240px)] w-[1200px] pointer-events-none hero-img_res">
@@ -30,7 +29,7 @@ const Hero = () => {
             />
           </div>
         </div>
-      </Element>
+      </div>
     </section>
   );
 };
