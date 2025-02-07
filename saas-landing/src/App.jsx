589a8293365d0a919a511005394bc869aa8a5c46
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2025-02-07 18:31:01 +0330

    debuging package  in site

---
+++ saas-landing/src/App.jsx
@@ -0,0 +1,21 @@
+import { Faq, Testimonials, Footer, Download, Header, Hero, Features, Pricing } from './sections'
+
+
+
+function App() {
+
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Faq />
+      <Testimonials />
+      <Download />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
